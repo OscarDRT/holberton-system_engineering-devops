abccb42d25b8e4503d6e4eb7d91a58f8e55eb1e3
oscar <oscarnetworkingpro@gmail.com>
2020-03-25 19:19:04 -0500

    add

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,22 +1,6 @@
 # Using Puppet
 
-exec { 'update':
-  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command => 'apt-get -y update'
-}
-exec { 'nginx':
-  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command => 'apt-get -y install nginx'
-}
-exec { 'index':
-  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command => "echo "Holberton School" > /var/www/html/index.html"
-}
-exec { 'config_redirect':
-  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command => "sed -i '/listen 80 default_server;/a rewrite ^/redirect_me https://www.linkedin.com/ permanent;' /etc/nginx/sites-available/default"
-}
-exec { 'start_nginx':
-  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command => 'service nginx start'
+exec { 'install nginx':
+  command  => "apt-get -y update && apt-get -y install nginx && echo 'Holberton School' > /var/www/html/index.html && sed -i '/listen 80 default_server;/a rewrite ^/redirect_me https://www.linkedin.com/ permanent;' /etc/nginx/sites-available/default && service nginx start"
+  provider => 'shell',
 }
