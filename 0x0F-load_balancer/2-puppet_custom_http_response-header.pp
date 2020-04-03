bf5cd323d8d6396f6da7ea470c19f1e1ae82f845
OscarDRT <oscarnetworkingpro@gmail.com>
2020-04-03 17:23:51 -0500

    PUPPET

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,7 +1,6 @@
 # Using Puppet
 
 exec { 'install nginx':
-  command  => "apt-get -y update && apt-get -y install nginx && echo 'Holberton School' > /var/www/html/index.html && sed -i '/sendfile on;/a add_header X-Served-By $hostname;' /etc/nginx/nginx.conf
- && service nginx start",
-  provider => 'shell',
+  command  => "sudo apt-get -y update && sudo apt-get -y install nginx %% echo "Holberton School" | sudo tee /var/www/html/index.html && sed -i '/sendfile on;/a add_header X-Served-By $hostname;' /etc/nginx/nginx.conf && service nginx start",
+  provider => "shell",
 }
