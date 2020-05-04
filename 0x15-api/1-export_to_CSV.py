64dca07405c13151ba9446695b8502668abf040f
OscarDRT <oscarnetworkingpro@gmail.com>
2020-05-04 14:42:10 -0500

    script to export data in the CSV format

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Use API"""
+
+import csv
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    response = requests.get(
+                            "https://jsonplaceholder.typicode.com/todos?userId={}"
+                            .format(argv[1])).json()
+
+    user_response = requests.get(
+                                "https://jsonplaceholder.typicode.com/users/{}"
+                                .format(argv[1])).json()
+
+
+    username = user_response['username']
+    user_id = argv[1]
+
+    with open(user_id + '.csv', 'w', newline='') as csvfile:
+        for item in response:
+            spamwriter = csv.writer(
+                                    csvfile, delimiter=',',
+                                    quotechar='"',
+                                    quoting=csv.QUOTE_ALL)
+            spamwriter.writerow(
+                                [item['userId']] +
+                                [username] +
+                                [item['completed']] +
+                                [item['title']])
